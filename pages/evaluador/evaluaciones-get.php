88bd2bf1a8980cb703ca4756d3d6a1d74a2d7f9c
Christiam Mendoza <mail@christiam.me>
2018-10-16 13:44:03 -0500

    Added required new fields to the query

--- pages/evaluador/evaluaciones-get.php
+++ pages/evaluador/evaluaciones-get.php
@@ -10,7 +10,7 @@ if ( isset($_SESSION['aes_username']) ) {
     // Search in database
 
     // To protect MySQL injection, create parametrized query
-    $evaluaciones = $DB_connection -> prepare("SELECT aes_assessment_evento.inicio, CONCAT(aes_estudiantes.nombre, ' ', aes_estudiantes.paterno) AS evaluado, aes_assessment_evento.lugar, IF(aes_resultados.id IS NULL,'pendiente','iniciado') AS status, aes_assessment_asignacion.evaluador, aes_assessment_evento.titulo FROM `aes_assessment_asignacion` LEFT JOIN `aes_assessment_evento` ON aes_assessment_evento.id = aes_assessment_asignacion.evento_id LEFT JOIN aes_estudiantes ON aes_estudiantes.correo = aes_assessment_asignacion.estudiante LEFT JOIN aes_resultados ON aes_resultados.plan_id = aes_assessment_asignacion.plan_id WHERE evaluador = :username ORDER BY aes_assessment_evento.inicio ASC");
+    $evaluaciones = $DB_connection -> prepare("SELECT aes_assessment_evento.inicio, CONCAT(aes_estudiantes.nombre, ' ', aes_estudiantes.paterno) AS evaluado, aes_estudiantes.correo, aes_assessment_evento.lugar, IF(aes_resultados.id IS NULL,'pendiente','iniciado') AS status, aes_assessment_asignacion.evaluador, aes_assessment_evento.titulo, aes_assessment_asignacion.evento_id FROM `aes_assessment_asignacion` LEFT JOIN `aes_assessment_evento` ON aes_assessment_evento.id = aes_assessment_asignacion.evento_id LEFT JOIN aes_estudiantes ON aes_estudiantes.correo = aes_assessment_asignacion.estudiante LEFT JOIN aes_resultados ON aes_resultados.plan_id = aes_assessment_asignacion.plan_id WHERE evaluador = :username ORDER BY aes_assessment_evento.inicio ASC");
 
     // Execute query
     $evaluaciones -> execute(
@@ -24,10 +24,12 @@ if ( isset($_SESSION['aes_username']) ) {
         $data[] = array(
           'fechahora' => $row['inicio'],
           'estudiante' => $row['evaluado'],
+          'email' => rawurlencode(base64_encode($row['correo'])),
           'sala' => $row['lugar'],
           'estado' => $row['status'],
           'evaluador' => $row['evaluador'],
-          'evento' => $row['titulo']
+          'evento' => $row['titulo'],
+          'id_evento' => $row['evento_id']
         );
     }
 
