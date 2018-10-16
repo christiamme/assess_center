f74bdc8d36a1429bfb01e6a3793e2dc9ee24122b
Christiam Mendoza <mail@christiam.me>
2018-10-16 17:44:31 -0500

    Just some clean-up of the file

--- pages/evaluador/evaluador.js
+++ pages/evaluador/evaluador.js
@@ -38,29 +38,4 @@ $(document).ready(function () {
 // Logout
 document.getElementById("signOut").onclick = function () {
   window.location.href = "../login/login.php";
-  // firebase.auth().signOut().then(function() {
-  //   // Sign-out successful
-  //   userinfo = {
-  //     name: "Nombre",
-  //     email: "Correo",
-  //     photoUrl: "../../vendors/theme/img/avatar2.png",
-  //     emailVerified: false,
-  //     uid: 0
-  //   }
-  //   alert("Cerraste sesión");
-  // }).catch(function(error) {
-  //   // An error happened
-  //   alert("Hubo un error al cerrar tu sesión, vuelve a intentar.");
-  // });
 };
-
-/*
-for (var i = 0; i < json[0].length; i++) {
-    tr = $('<tr/>');
-    tr.append('<td><a href="../cliente/cliente.php?id=' + json[0][i].id + '">' + json[0][i].nomcomercial + '</a></td>');
-    tr.append('<td>' + json[0][i].razon_social + '</td>');
-    tr.append('<td>' + json[0][i].tel1 + '</td>');
-    tr.append('<td> - </td>');
-    $('#tabla_clientes').append(tr);
-}
- */
